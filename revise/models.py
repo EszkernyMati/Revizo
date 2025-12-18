36a641759bee932df362f760a1c8d6ab876816bb
NikoZzz04 <nikodemek2141@gmail.com>
2025-12-18 14:34:21 +0100

    validations for cars adding

--- revise/models.py
+++ revise/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+
+plate_validator = RegexValidator(
+    regex=r'^[A-Z0-9]{2,10}$', 
+    message="Numer rejestracyjny musi składać się z 2-10 dużych liter lub cyfr (bez spacji)."
+)
+
+vin_validator = RegexValidator(
+    regex=r'^[A-HJ-NPR-Z0-9]{17}$',
+    message="Niepoprawny format VIN (17 znaków, wykluczając litery I, O, Q)."
+)
 
 class Car(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cars')
@@ -8,8 +19,8 @@ class Car(models.Model):
     model_name = models.CharField(max_length=100) 
     year = models.DateField()           
     engine = models.CharField(max_length=100)       
-    license_plate = models.CharField(max_length=20, unique=True) 
-    vin = models.CharField(max_length=17, unique=True)           
+    license_plate = models.CharField(max_length=20, unique=True, validators=[plate_validator]) 
+    vin = models.CharField(max_length=17, unique=True,  validators=[vin_validator])           
 
     def __str__(self):
         return f"{self.brand} {self.model_name} ({self.license_plate})"
--- revise/serializers.py
+++ revise/serializers.py
@@ -2,12 +2,33 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import *
 
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ['id', 'brand', 'model_name', 'year', 'engine', 'license_plate', 'vin']
+        read_only_fields = ['id'] 
+    def validate_vin(self, value):
+        if len(value) != 17:
+            raise serializers.ValidationError("Numer VIN musi mieć dokładnie 17 znaków.")
+        return value
+    def validate_year(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("Data produkcji nie może być późniejsza od teraźniejszości")
+        return value
+
+    def validate_license_plate(self, value):
+        clean_plate = value.replace(" ", "").upper()
+        if len(clean_plate) < 4:
+            raise serializers.ValidationError("Numer rejestracyjny jest za krótki.")
+        return clean_plate
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    cars = CarSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ("id", "username", "password", "email")
+        fields = ("id", "username", "password", "email", "cars")
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -16,9 +37,3 @@ class UserSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = ['id', 'brand', 'model_name', 'year', 'engine', 'license_plate', 'vin']
-        read_only_fields = ['id'] 
